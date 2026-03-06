9416702f78960e0edacad5d85db7066e54056169
SujalPatil21 <sujalbpatil21@gmail.com>
2026-03-07 05:07:19 +0530

    full

---
+++ GeoWatch - Application/lib/core/constants/api_constants.dart
@@ -0,0 +1,12 @@
+class ApiConstants {
+
+  // Backend server running on your laptop
+  // IP taken from ipconfig (Wi-Fi IPv4 Address)
+
+  static const String baseUrl = 'http://10.96.130.210:8080';
+
+  // APIs
+  static const String nearbyEvents = '/api/events/nearby';
+
+  static const String incidents = '/api/incidents';
+}
